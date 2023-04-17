1162437e5dfa9f2c327a4f417f08f18520d28901
Carson <carson.sturtevant@gmail.com>
2023-04-17 10:56:18 -0600

    Create entity project with a couple entities

---
+++ TheGreatestApiInTheWorldTribute.Entities/Models/Band.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace TheGreatestApiInTheWorldTribute.Entities.Models
+{
+    public class Band
+    {
+        [Column("BandId")] 
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Band name is a required field.")]
+        [MaxLength(60, ErrorMessage = "Maximum length for the Name is 60 characters.")] 
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Hometown is a required field.")]
+        [MaxLength(60, ErrorMessage = "Maximum length for the Hometown is 60 characters.")] 
+        public string? Hometown { get; set; }
+
+        public ICollection<Member>? Members { get; set; }
+    }
+}
---
+++ TheGreatestApiInTheWorldTribute.Entities/Models/Member.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace TheGreatestApiInTheWorldTribute.Entities.Models
+{
+    public class Member
+    {
+        [Column("MemberId")] 
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Member name is a required field.")]
+        [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")] 
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Age is a required field.")] 
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Role is a required field.")]
+        [MaxLength(20, ErrorMessage = "Maximum length for the Role is 20 characters.")] 
+        public string? Role { get; set; }
+
+        [ForeignKey(nameof(Band))] 
+        public Guid BandId { get; set; }
+
+        public Band? Band { get; set; }
+    }
+}
